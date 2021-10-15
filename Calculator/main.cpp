c292fdf4de1220e5525f825bc89ca78f32281e98
Majkel <majkelkrol@icloud.com>
2021-10-15 13:54:27 +0200

    Add files

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -6,9 +6,35 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    
+    int numb1, numb2;
+    char q;
+
+    cout << "Enter first number: ";
+    cin >> numb1;
+    
+    cout << "Enter operator: ";
+    cin >> q;
+    
+    cout << "Enter secound number: ";
+    cin >> numb2;
+    
+    int result;
+    if (q == '+') {
+        result = numb1 + numb2;
+    } else if (q == '-') {
+        result = numb1 - numb2;
+    } else if (q == '*') {
+        result = numb1 * numb2;
+    } else if (q == '/') {
+        result = numb1 / numb2;
+    } else {
+        cout << "Invalid operation";
+    }
+    cout << "The result: " << result << endl;
+
     return 0;
 }
